83174f14ae09e79acb3722f98fb9a3de6076b5e3
Aditya Jadhav <aditya.s.jadhav07@gmail.com>
2026-09-11 20:50:46 +0530

    Java Programs Practice. Start from Basics
    Signed-off-by: Aditya

---
+++ src/main/java/starPatterns/StarPattern1.java
@@ -0,0 +1,16 @@
+package starPatterns;
+
+public class StarPattern1
+{
+    static void main()
+    {
+        for (int i=1; i<=5; i++)
+        {
+            for (int j=1; j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
